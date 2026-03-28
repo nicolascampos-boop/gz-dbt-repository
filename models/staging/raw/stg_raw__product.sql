d14b9cbbbf297c68b2360083d76442cbd199cf13
nicolas.campos9810@gmail.com <nicolas.campos9810@gmail.com>
2026-03-28 20:22:26 +0000

    .

--- models/staging/raw/stg_raw__product.sql
+++ models/staging/raw/stg_raw__product.sql
@@ -10,7 +10,16 @@ renamed as (
 
     select
         products_id,
-        CAST(purchase_price AS FLOAT64) AS purchase_price
+          - name: product
+        identifier: raw_gz_product
+        description: "Products sold by Greenweez"
+        columns:
+          - name: products_id
+            description: "Unique product identifier"
+            tests:
+              - unique
+              - not_null
+        CAST(purchase_price AS FLOAT64) AS 
 
     from source
 
