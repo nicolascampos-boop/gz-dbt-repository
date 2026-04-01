a20098252e13fc6f186579f0bebbe2ce589c8a91
Nicolás Campos <nicolas.campos9810@gmail.com>
2026-04-01 00:27:49 +0000

    new commit

--- models/mart/finance/finance_days.sql
+++ models/mart/finance/finance_days.sql
@@ -17,9 +17,7 @@ quantity_per_order as (
     select
         orders_id,
         SUM(quantity) as quantity
-
     from sales_margin
-
     group by orders_id
 
 ),
@@ -36,9 +34,7 @@ joined as (
         orders_operational.ship_cost,
         orders_operational.operational_margin,
         quantity_per_order.quantity
-
     from orders_operational
-
     left join quantity_per_order
         on orders_operational.orders_id = quantity_per_order.orders_id
 
@@ -48,17 +44,15 @@ daily as (
 
     select
         date_date,
-        COUNT(DISTINCT orders_id)            as nb_transactions,
-        SUM(revenue)                         as revenue,
+        COUNT(DISTINCT orders_id)                as nb_transactions,
+        SUM(revenue)                             as revenue,
         SUM(revenue) / COUNT(DISTINCT orders_id) as average_basket,
-        SUM(operational_margin)              as operational_margin,
-        SUM(purchase_cost)                   as purchase_cost,
-        SUM(shipping_fee)                    as shipping_fee,
-        SUM(ship_cost)                       as ship_cost,
-        SUM(quantity)                        as quantity
-
+        SUM(operational_margin)                  as operational_margin,
+        SUM(purchase_cost)                       as purchase_cost,
+        SUM(shipping_fee)                        as shipping_fee,
+        SUM(ship_cost)                           as ship_cost,
+        SUM(quantity)                            as quantity
     from joined
-
     group by date_date
 
 )
